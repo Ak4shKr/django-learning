7f07428ddaf0ba4ca04bc81d42a1db2cad61b37f
ak4shkr <akash.kumar@crosslynxus.com>
2025-09-04 15:08:43 +0530

    added and verified jwt based login authentication

--- djangoPractice/books/models.py
+++ djangoPractice/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -12,17 +13,36 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = password 
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=15, null=True)
+    name = models.CharField(max_length=20)
     phone = models.BigIntegerField(
         validators=[
-            MinValueValidator(1000000000),   # minimum 10-digit number
-            MaxValueValidator(9999999999)    # maximum 10-digit number
+            MinValueValidator(1000000000),
+            MaxValueValidator(9999999999),
         ]
     )
-    is_manager=models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "phone"]
 
     def __str__(self):
-        return self.name
+        return self.email
+
